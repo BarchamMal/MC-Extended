696b3e6a503d3d1dd64ace8385292f88f28d6785
BarchamMal <115943779+BarchamMal@users.noreply.github.com>
2023-05-18 10:31:47 -0500

    add the files

---
+++ src/client/java/barch/mc_extended/ExampleModClient.java
@@ -0,0 +1,10 @@
+package barch.mc_extended;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
