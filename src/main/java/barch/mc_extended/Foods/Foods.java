0655e04e65cef3a045cd628496d1ce3b1e370f36
BarchamMal <115943779+BarchamMal@users.noreply.github.com>
2023-09-21 14:40:01 -0500

    Add spinach
    Done

--- src/main/java/barch/mc_extended/Foods/Foods.java
+++ src/main/java/barch/mc_extended/Foods/Foods.java
@@ -11,6 +11,7 @@ public class Foods {
     public static final Tomato TOMATO = Tomato.INSTANCE;
     public static final Onion ONION = Onion.INSTANCE;
     public static final Meats MEATS = Meats.INSTANCE;
+    public static final Spinach SPINACH = Spinach.INSTANCE;
     public static final Specialty SPECIALTY = Specialty.INSTANCE;
 
 
@@ -28,6 +29,10 @@ public class Foods {
         // register the onion stuff
         ONION.RegisterAll();
         ONION.GroupItems();
+
+        // register the spinach stuff
+        SPINACH.RegisterAll();
+        SPINACH.GroupItems();
 
         // register cheese
         CHEESE.RegisterAll();
--- src/main/java/barch/mc_extended/Foods/Specialty.java
+++ src/main/java/barch/mc_extended/Foods/Specialty.java
@@ -19,11 +19,13 @@ public class Specialty {
 
     public static final FoodComponent ONION_SAUCE_FOOD_COMPONANT = new FoodComponent.Builder().hunger(9).saturationModifier(8f).build();
     public static final FoodComponent BACON_AND_MUSHROOMS_FOOD_COMPONENT = new FoodComponent.Builder().hunger(12).saturationModifier(12).build();
+    public static final FoodComponent CHEESY_SAAG_FOOD_COMPONENT = new FoodComponent.Builder().hunger(18).saturationModifier(20).build();
 
 
 
     public static final Item ONION_TOMATO_SAUCE = new ContainedFood(new FabricItemSettings().food(ONION_SAUCE_FOOD_COMPONANT), Items.BOWL);
     public static final Item BACON_AND_MUSHROOMS = new ContainedFood(new FabricItemSettings().food(BACON_AND_MUSHROOMS_FOOD_COMPONENT).maxCount(16), Items.BOWL);
+    public static final Item CHEESY_SAAG = new ContainedFood(new FabricItemSettings().food(CHEESY_SAAG_FOOD_COMPONENT).maxCount(16), Items.BOWL);
 
 
     public static void RegisterAll() {
@@ -34,12 +36,14 @@ public class Specialty {
 
         ItemGrouper.GroupItem(ONION_TOMATO_SAUCE, new ItemGrouped[]{new ItemGrouped(ItemGroups.FOOD_AND_DRINK, Items.RABBIT_STEW)});
         ItemGrouper.GroupItem(BACON_AND_MUSHROOMS, new ItemGrouped[]{new ItemGrouped(ItemGroups.FOOD_AND_DRINK, ONION_TOMATO_SAUCE)});
+        ItemGrouper.GroupItem(CHEESY_SAAG, new ItemGrouped[]{new ItemGrouped(ItemGroups.FOOD_AND_DRINK, BACON_AND_MUSHROOMS)});
 
     }
 
     public static void RegisterItems() {
         Registry.register(Registries.ITEM, new Identifier(NAMESPACE, "onion_tomato_sauce"), ONION_TOMATO_SAUCE);
         Registry.register(Registries.ITEM, new Identifier(NAMESPACE, "bacon_and_mushrooms"), BACON_AND_MUSHROOMS);
+        Registry.register(Registries.ITEM, new Identifier(NAMESPACE, "cheesy_saag"), CHEESY_SAAG);
 
     }
 
